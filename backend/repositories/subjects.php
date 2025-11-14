687cb9628ed4e8d0798b88629389817d4384f2a2
tomassacadura <148502251+tomassacadura@users.noreply.github.com>
2025-11-14 19:05:40 +0000

    agregado de funcion  getSubjectByName para poder distinguir tipo de error

--- backend/repositories/subjects.php
+++ backend/repositories/subjects.php
@@ -46,6 +46,15 @@ function getSubjectById($conn, $id)
     return $result->fetch_assoc(); 
 }
 
+
+function getSubjectByName($conn, $name) {
+    $sql = "SELECT * FROM subjects WHERE name = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $name);
+    $stmt->execute();
+    return $stmt->get_result()->fetch_assoc();
+}
+
 function createSubject($conn, $name) 
 {
     // Verificar si ya existe una materia con ese nombre
